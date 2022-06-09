c23db5d8c18c55b98e1c0ceb9c4e2b75e1729f0c
Letcher20 <ericklauren86@gmail.com>
2022-06-09 21:44:07 +0700

    Sensor added

--- app/src/main/java/com/example/a160419095_advancenativeuts/view/MainActivity.kt
+++ app/src/main/java/com/example/a160419095_advancenativeuts/view/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.a160419095_advancenativeuts.view
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
@@ -11,11 +17,20 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.a160419095_advancenativeuts.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    //Untuk sensor cahaya
+    private var lightSensor: Sensor? = null
+    private var lightReading = 0f
+
     private lateinit var navController: NavController
+
+    private lateinit var  sensorManager: SensorManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Sensor Cahaya
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
 
@@ -24,7 +39,37 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setupWithNavController(navController)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        //cek sensor cahaya
+        if(lightSensor == null){
+            Toast.makeText(this, "No Light sensor Detected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(drawerLayout) || super.onSupportNavigateUp()
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        p0?.let {
+            when(it.sensor.type){
+
+                Sensor.TYPE_LIGHT->{
+                    lightReading = it.values[0]
+
+                    if(lightReading <=10){
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }else{
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
 }
