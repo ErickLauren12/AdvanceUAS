477489b287288ce37f1273706487cbbe9f5e30b4
andreas <s160419047@student.ubaya.ac.id>
2022-06-27 21:42:24 +0700

    auth view model

--- app/src/main/java/com/example/a160419095_advancenativeuts/viewmodel/AccountViewModel.kt
+++ app/src/main/java/com/example/a160419095_advancenativeuts/viewmodel/AccountViewModel.kt
@@ -16,8 +16,6 @@ class AccountViewModel(application: Application) : AndroidViewModel(application)
 //    val loadingLiveData = MutableLiveData<Boolean>()
 //    val cartLoadErrorLiveData = MutableLiveData<Boolean>()
     private var job = Job()
-    override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
 
     fun register(list: List<Account>) {
         launch {
@@ -26,6 +24,9 @@ class AccountViewModel(application: Application) : AndroidViewModel(application)
 
         }
     }
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     fun login(username:String, pass:String) {
         launch {
